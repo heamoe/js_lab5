b7cc802d028d8e1229af62a33e0803c0a1d2e8a3
Amos <60774863+heamoe@users.noreply.github.com>
2024-10-02 14:23:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -14,4 +14,4 @@ console.log(minMax([1, 2, 3, 4, 5]));
 // Challenge 3
 ((length, width) => {
   console.log(`The area of a rectangle with a length of ${length} and a width of ${width} is ${length * width}.`);
-})(10, 5);
+})(15, 7);
